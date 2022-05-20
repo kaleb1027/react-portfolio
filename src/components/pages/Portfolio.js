95408e1fd85c90f66f7b785a5f323bbae4ce44ec
Kaleb Sherrill <kaleb1027@gmail.com>
2022-05-20 05:36:26 -0400

    Added portfolios and styles to portfolio divs

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,29 +1,69 @@
 import React from 'react';
+import "./Portfolio.css"
 
-const styles = {
-  align: {
-    textAlign: "center"
-  }
-}
 export default function Portfolio() {
   return (
-    <div style={styles.align}>
-      <h1>Portfolio Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+    
+    <div>
+      <section class="section-area" id="projects">
+        <div class="div-container">
+          <a
+            href="https://github.com/kaleb1027/nostalgia-tunes"
+            class="box-item refactor"
+            id="project-1"
+          >
+            <div className="project-name-text">
+              <h3>Nostalgia Tunes</h3>
+              <span>HTML/CSS/JS</span>
+            </div>
+          </a>
+
+          <a
+            href="https://github.com/kaleb1027/note-taker"
+            class="box-item led"
+            id="project-2"
+          >
+            <div className="project-name-text">
+              <h2>Note Taker</h2>
+              
+              <span>HTML/CSS/JS</span>
+            </div>
+          </a>
+
+          <a
+            href="https://github.com/kaleb1027/work-day-scheduler"
+            class="box-item calc"
+            id="project-3"
+          >
+            <div className="project-name-text">
+              <h3>Work Day scheduler</h3>
+              <span>HTML/CSS/JS</span>
+            </div>
+          </a>
+
+          <a
+            href="https://github.com/kaleb1027/password-generator"
+            class="box-item puzzles"
+            id="project-4"
+          >
+            <div className="project-name-text">
+              <h3>Password Generator</h3>
+              <span>HTLM/CSS/JS</span>
+            </div>
+          </a>
+
+          <a
+            href="https://github.com/kaleb1027/code-quiz"
+            class="box-item buddy"
+            id="project-5"
+          >
+            <div className="project-name-text">
+              <h3>Code Quiz</h3>
+              <span>HTML/CSS</span>
+            </div>
+          </a>
+        </div>
+      </section>
     </div>
   );
 }
