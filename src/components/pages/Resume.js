3226c9578a5238c35251890d978b7c96aedf4217
Kaleb Sherrill <kaleb1027@gmail.com>
2022-05-20 06:02:39 -0400

    Added resume to resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -10,16 +10,7 @@ export default function Resume() {
     <div style={styles.align}>
       <h1>Resume Page</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
+        Click <a href="https://drive.google.com/file/d/1NnS_BR2HfohqzXPzRvwG_z6gu4cgkxdW/view?usp=sharing">HERE</a> for my resume!
       </p>
     </div>
   );
